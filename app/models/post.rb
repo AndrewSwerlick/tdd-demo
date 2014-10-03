7a85f11db17ed08893586837f649eb5a6387c133
Andrew Swerlick <andrew.swerlick@hbdi.com>
2014-10-02 20:17:58 -0400

    hardcoded list of bad words

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,12 +2,27 @@
 #
 # Table name: posts
 #
-#  id          :integer          not null, primary key
-#  name        :string(255)
-#  description :text
-#  created_at  :datetime
-#  updated_at  :datetime
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  content    :text
+#  created_at :datetime
+#  updated_at :datetime
 #
 
 class Post < ActiveRecord::Base
+
+  def filtered_content
+    filtered = self.content
+    bad_words.each do |words|
+      filtered = filtered.gsub(words[0],words[1])
+    end
+    filtered
+  end
+
+  def bad_words
+    [
+      [/damn/i,"darn"],
+      [/crap/i,"poop"]
+    ]
+  end
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -15,4 +15,54 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "that we can get the filtered content of a post" do
+    # Arrange
+    post = Post.new
+    post.content = "This is some g-rated content"
+
+    # Act
+    filtered_content = post.filtered_content
+
+    # Assert
+    assert_not_nil filtered_content
+  end
+
+  test "that if there are no swear words the filtered content is the same as normal content" do
+    # Arrange
+    post = Post.new
+    post.content = "This is some g-rated content"
+
+    # Act
+    filtered_content = post.filtered_content
+
+    # Assert
+    assert_equal post.content, post.filtered_content
+  end
+
+  test "that if there is a swear word it gets replaced" do
+    # Arrange
+    post = Post.new
+    post.content = "This content is damn filthy"
+
+    # Act
+    filtered_content = post.filtered_content
+
+    # Assert
+    assert !(filtered_content =~ /damn/), "found damn"
+    assert filtered_content =~ /darn/, "didn't find darn"
+  end
+
+  test "that if there are multiple swear words they get replaced" do
+    # Arrange
+    post = Post.new
+    post.content = "Crap this was a mistake"
+
+    # Act
+    filtered_content = post.filtered_content
+
+    # Assert
+    assert !(filtered_content =~ /crap/i), "found crap"
+    assert filtered_content =~ /poop/i, "didn't find poop"
+  end
 end
