e8f4fb2a2d156ca6230398c3da25fa9e7992dbba
Andrew Swerlick <andrew.swerlick@hbdi.com>
2015-01-17 15:57:48 -0500

    pushing up modified schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20141002231750) do
 
   create_table "posts", force: true do |t|
     t.string   "name"
-    t.text     "description"
+    t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
